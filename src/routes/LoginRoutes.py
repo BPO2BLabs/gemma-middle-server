7b9030afc1df0636e24e91af6b6df42c674b460f
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-12-20 15:34:44 -0500

    [add] login to orchestrator

--- src/routes/LoginRoutes.py
+++ src/routes/LoginRoutes.py
@@ -25,8 +25,8 @@ def to_orchestrator():
     print("enc_pass",encrypted_pass)
     encrypted_pass_b64 = base64.b64encode(encrypted_pass).decode('utf-8')
     form_data ={
-    "userName": "daniel.gomez@bpo2b.com",
-    "password": "UNbDEa/Aacz/Csj7NJyfMu2DL2GZHWXVUFHzIMUy1h+H+X4y5aNv9Mh4RHQ9EhJ8HstT6Z52Zox5/dV4x4uK88K6MItfdnEOd8P8oHer2mSaelG7k0Y87t365WKZx0QwM5NJJ2OBK0hJCipe6KpFw4YJiAnKVsE90KPmdGbUDQCZWPHlzYmzdc6daqUUGiI/ohQuqw7q5orffVgo+3c8vK7t0rMm/HMizpK8Gr/9zWPKcS8+Rz4t21si/+e0VFVWbNn1iJ7v7F3G+wHVLhafLjcTpm9uQfNrv1YdnIdJF9jLnYPilKhGi+gVn+Xot5q2yEAYDHxPw0Ei0ytE2YhgCA=="
+    "userName": GEMMA_USER,
+    "password": encrypted_pass_b64
     }
     print(form_data)
     headers = {"Content-Type": "application/json"}
