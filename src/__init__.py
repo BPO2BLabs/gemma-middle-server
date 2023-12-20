e92d66f371115079c1abbc2455124454ceca92f9
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-12-20 12:09:00 -0500

    [add] login to orchestrator

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 # Routes
 from .routes import TranscriptRoutes
 from .routes import PingRoutes
+from .routes import LoginRoutes
 
 app = Flask(__name__)
 CORS(app)
@@ -16,5 +17,6 @@ def init_app(config):
     # Blueprints
     app.register_blueprint(TranscriptRoutes.main, url_prefix='/transcripts')
     app.register_blueprint(PingRoutes.main, url_prefix='/ping')
-
+    app.register_blueprint(LoginRoutes.main, url_prefix='/login')
+    
     return app
---
+++ src/routes/LoginRoutes.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, request, jsonify
+import os
+import requests
+from dotenv import load_dotenv
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+load_dotenv()
+
+main = Blueprint('login_blueprint', __name__)
+
+GEMMA_USER = os.getenv('GEMMA_USER')
+GEMMA_PASS = os.getenv('GEMMA_PASS')
+URL_BACKEND = os.getenv('URL_BACKEND')
+PEM_PATH = os.getenv('PEM_PATH')
+
+with open(PEM_PATH, "rb") as key_file:
+    public_key = serialization.load_pem_public_key(
+        key_file.read(),
+        backend=default_backend()
+    )
+
+@main.route('/toorchestrator', methods=['POST'])
+def to_orchestrator():
+    encrypted_pass = public_key.encrypt(
+        GEMMA_PASS.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+        ),
+    )
+    form_data = {"username":GEMMA_USER,"password":encrypted_pass}
+    response = requests.post(f"{URL_BACKEND}/login", data=form_data)
+    return response.json()
+    
